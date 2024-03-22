c55ca6b5f43aa214c681916d846c42ee3e3a33ff
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-03-22 05:48:56 +0330

    customize data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import {books} from './constants/mockData'
+
 function App() {
   return (
     <>
       <h1>Book App Starting files</h1>
+      {books.map((book) => (<p>{book.title}</p>))}
     </>
   );
 }
