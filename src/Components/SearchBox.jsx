39216db8fe74ca739994a30569e374461c14b4e0
mohammadrasoulasghari <mohammadrasoulasghari1@gmail.com>
2024-03-22 12:59:23 +0330

    add style to search box

--- src/Components/SearchBox.jsx
+++ src/Components/SearchBox.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 
+import styles from "./SearchBox.module.css"
+
 function SearchBox({search,setSearch , searchHandler}) {
     
     return (
-        <div>
+        <div className={styles.search}>
             <input value={search} onChange={ (e) =>setSearch(e.target.value.toLocaleLowerCase())} type="text" placeholder='Search title' />
             <button onClick={searchHandler}><IoSearch /></button>
         </div>
